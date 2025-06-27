a1f3387e6ada1102974c528ec82c733b4977d338
matthewzegekwe <matthewzegekwe@gmail.com>
2025-06-27 21:17:54 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './components/main.jsx';
 import Login from './components/Login.jsx';
 import Register from './components/Register.jsx';
-import Profile from './components/Profile.jsx';
 import './components/style.css';
 import { AuthProvider } from "./components/context/AuthProvider.jsx";
 
@@ -17,7 +16,6 @@ const App = () => {
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
         </Routes>
       </AuthProvider>
     </Router>
